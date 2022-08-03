fea3e55579d5c0b557012e19ed5d4fdb28f57990
wuxiangege <wuxiangege@gmail.com>
2022-08-03 18:35:33 +0800

    在hptx代理模式下的zorm使用示例

---
+++ http_proxy_zorm/aggregation_svc/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"gitee.com/chunanyong/zorm"
+	"github.com/cectc/hptx"
+	"github.com/cectc/hptx/pkg/tm"
+	"github.com/gin-gonic/gin"
+
+	"github.com/cectc/hptx-samples/aggregation_svc/svc"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var dbDao *zorm.DBDao
+
+func InitDbByZorm(db *sql.DB) error {
+	dbConfig := zorm.DataSourceConfig{
+		//连接数据库DSN
+		DSN: "root:Wuxian091228@tcp(rm-bp1d4145wfeb6z4stso.mysql.rds.aliyuncs.com:3306)/order?charset=utf8&parseTime=true", //MySQL
+		//数据库类型
+		// DriverName: "dm",
+		// DBType:     "dm",
+		DriverName: "mysql",
+		DBType:     "mysql",
+		//是否的打印sql
+		PrintSQL: true,
+		//最大连接数 默认50
+		MaxOpenConns: 0,
+		//最大空闲数 默认50
+		MaxIdleConns: 0,
+		//连接存活秒时间. 默认600
+		ConnMaxLifetimeSecond: 0,
+		//事务隔离级别的默认配置,默认为nil
+		DefaultTxOptions: nil,
+	}
+	if db != nil {
+		dbConfig.DSN = ""
+		dbConfig.SQLDB = db
+	}
+
+	var err error
+	dbDao, err = zorm.NewDBDao(&dbConfig)
+	if err != nil {
+		log.Fatalf("数据库连接异常 %v", err)
+		return err
+	}
+
+	log.Println("数据库连接成功")
+	return nil
+}
+
+func main() {
+	r := gin.Default()
+
+	configPath := os.Getenv("ConfigPath")
+	hptx.InitFromFile(configPath)
+	tm.Implement(svc.ProxySvc)
+
+	err := InitDbByZorm(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	r.GET("/createSoCommit", func(c *gin.Context) {
+		err := svc.ProxySvc.CreateSo(c, false)
+		if err == nil {
+			c.JSON(200, gin.H{
+				"success": true,
+				"message": "success",
+			})
+		} else {
+			c.JSON(400, gin.H{
+				"success": false,
+				"message": err.Error(),
+			})
+		}
+	})
+
+	r.GET("/createSoRollback", func(c *gin.Context) {
+		err := svc.ProxySvc.CreateSo(c, true)
+		if err == nil {
+			c.JSON(200, gin.H{
+				"success": true,
+				"message": "success",
+			})
+		} else {
+			c.JSON(400, gin.H{
+				"success": false,
+				"message": err.Error(),
+			})
+		}
+	})
+
+	r.Run(":8003")
+}
